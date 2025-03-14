900cffbceb484db8e326f8e755a396f8e3b32642
Himanshu Singh <77524402+Himanshucwu20@users.noreply.github.com>
2025-03-15 01:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pybase64
 
 app = Flask(__name__)
 app.secret_key = 'Ironman@2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://swms_db_user:v5CQMGK4WbZsmqw1tp4FS5vb6VRvtRPz@dpg-cva6pnqj1k6c739fkvog-a.oregon-postgres.render.com/swms_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smart_db_ag8j_user:fqnTV0rvMgxSOqNJNKd2NWmT7vv2BSO6@dpg-cva82edumphs73fpodr0-a.oregon-postgres.render.com/smart_db_ag8j'
 db = SQLAlchemy(app)
 api = Api(app)
 # postgresql://waste_management_system_database_5guq_user:zqeLkrTQqoGh9UyaxQQJgB3Ncmp4ceUz@dpg-cg6s9jl269v5l67ata4g-a.oregon-postgres.render.com/waste_management_system_database_5guq
